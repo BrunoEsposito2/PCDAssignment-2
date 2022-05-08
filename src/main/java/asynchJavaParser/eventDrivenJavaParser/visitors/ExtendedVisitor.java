6b09518df5e3f7619f9e0543b2fe25e8145277cc
BrunoEsposito2 <bruno.esposito2@studio.unibo.it>
2022-05-08 17:21:10 +0200

    minor Visitor refactoring

--- src/main/java/asynchJavaParser/eventDrivenJavaParser/visitors/ExtendedVisitor.java
+++
@@ -1,9 +0,0 @@
-package asynchJavaParser.eventDrivenJavaParser.visitors;
-
-import com.github.javaparser.ast.body.FieldDeclaration;
-
-public interface ExtendedVisitor extends Visitor {
-
-    void visit(FieldDeclaration fd, Void collector);
-
-}
